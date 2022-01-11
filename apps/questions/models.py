d3e9ac760cf590c79640c1268c6ce2eaa3f01cbf
castleq90 <castleq37@gmail.com>
2022-01-12 00:39:09 +0900

    [Add] question 앱 model작성완료

--- apps/questions/models.py
+++ apps/questions/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import User
+
+class Question(models.Model):
+    title      = models.CharField(max_length=200)
+    contents   = models.TextField()
+    like_count = models.PositiveIntegerField(default=0)
+    create_at  = models.DateTimeField(auto_now_add=True)
+    update_at  = models.DateTimeField(auto_now=True)
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'questions'
+
+class Comment(models.Model):
+    comment   = models.CharField(max_length=300)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    user      = models.ForeignKey(User, on_delete=models.CASCADE)
+    question  = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'comments'
+
+class Like(models.Model):
+    create_at  = models.DateTimeField(auto_now_add=True)
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    question   = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'likes'
