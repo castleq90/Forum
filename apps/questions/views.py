21aded7f2b1fa2f6025466f3a0b9ce486133edc1
castleq90 <castleq37@gmail.com>
2022-01-13 19:59:26 +0900

    [Add] 인가 권한 부여

--- apps/questions/views.py
+++ apps/questions/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import PermissionsMixin
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAuthenticated , IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from apps.questions.models import Question
 from apps.questions.serializers import QuestionSerializer
+from apps.questions.permissions import IsOwnerOrReadOnly
 
 class QuestionViewset(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
